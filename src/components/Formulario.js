c0d5d4108806973732b7efc3ac7e3b5362899ca8
makucg <chengokuu@gmail.com>
2021-05-30 12:38:36 +0200

    Initial commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Formulario = () => {
+    return ( 
+        <div className="bg-info">
+            <div className="container">
+                <div className="row">
+                    <form
+                        className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+                    >
+                        <fieldset>
+                            <legend className="text-center float-none">Buscador letras canciones</legend>
+                        </fieldset>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Formulario;
